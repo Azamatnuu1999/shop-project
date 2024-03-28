0bd4adc45bd8372c17829e0d6101550e79d27310
sarvar-murad <Sarvar.Murodov@powhr.co.uk>
2024-03-28 19:51:33 +0500

    changes

--- src/app/modules/product-detail/product-detail.component.ts
+++ src/app/modules/product-detail/product-detail.component.ts
@@ -7,11 +7,12 @@ import { CardService } from '../../core/services/card.service';
 import { takeUntil } from 'rxjs';
 import { DestroyService } from '../../core/services/destroy.service';
 import { CardComponent } from '../../components/card/card.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-product-detail',
     templateUrl: 'product-detail.component.html',
-    imports: [NgFor, CurrencyPipe, RouterLink, CardComponent],
+    imports: [NgFor, CurrencyPipe, RouterLink, CardComponent, ReactiveFormsModule],
     providers: [CardService, DestroyService],
     standalone: true
 })
@@ -27,16 +28,22 @@ export class ProductDetailComponent implements OnInit {
     };
     public similarProducts!: CardModel[];
     public mainUrl!: string;
+    public isSendClicked = false;
 
+    private fb = inject(FormBuilder);
     private route = inject(ActivatedRoute);
     private destoyer = inject(DestroyService);
     private $productService = inject(CardService);
     
-
     ngOnInit(): void {
         const productId = this.route.snapshot.params['id'];
         this.getProductData(productId);
     }
+
+    form: FormGroup = this.fb.group({
+        fullName: ['', Validators.required],
+        mobilePhone: ['', Validators.required]
+    })
 
     onGetData(id: string) {
         this.getProductData(id);
@@ -55,6 +62,26 @@ export class ProductDetailComponent implements OnInit {
             this.similarProducts = (data.cards as CardModel[]).filter(card => true);
             this.mainUrl = this.data.imgSrc[0];
         })
+    }
+
+    onSendData() {
+        this.isSendClicked = true;
+        if(this.form.invalid) {
+          this.updateValueAndValidity();
+        } else {
+          this.isSendClicked = false;
+          this.form.reset();
+        }
+    }
+    
+    updateValueAndValidity() {
+        Object.values(this.form.controls).forEach((control) => {
+          if(control.invalid) {
+            control.markAsDirty();
+            control.updateValueAndValidity({ onlySelf: true })
+          }
+        })
+        return
     }
 
 }
--- src/app/sections/message-section/message-section.component.ts
+++ src/app/sections/message-section/message-section.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-message-section',
   standalone: true,
-  imports: [],
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './message-section.component.html'
 })
-export class MessageSectionComponent {
+export class MessageSectionComponent implements OnInit {
+  public isSendClicked = false;
+
+  private fb = inject(FormBuilder);
+  public form: FormGroup = this.fb.nonNullable.group({
+    message: ['', Validators.required],
+    fullName: ['', Validators.required],
+    mobilePhone: ['', Validators.required]
+  });
+
+  ngOnInit() : void {
+
+  }
+
+  onSendMessage() {
+    this.isSendClicked = true;
+    if(this.form.invalid) {
+      this.updateValueAndValidity();
+    } else {
+      this.isSendClicked = false;
+      this.form.reset();
+    }
+  }
+
+  updateValueAndValidity() {
+    Object.values(this.form.controls).forEach((control) => {
+      if(control.invalid) {
+        control.markAsDirty();
+        control.updateValueAndValidity({ onlySelf: true })
+      }
+    })
+    return
+  }
 
 }
